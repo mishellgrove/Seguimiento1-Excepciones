660bf0720ebc2f5e82b9cadee72dd202bf6f2f29
Mishell Arboleda <mishell.arboleda1@correo.icesi.edu.co>
2020-08-21 23:31:11 -0500

    first commit

---
+++ src/model/Person.java
@@ -0,0 +1,14 @@
+package model;
+
+public class Person {
+	
+	public final static int TI = 1;
+	public final static int CC = 7;
+	public final static int PP = 7;
+	public final static int CE = 7;
+	private String type;
+	private int identification;
+	
+
+
+}
